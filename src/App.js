fe89ad1e45028ea3e0e31278c62c2009fbf08faf
yahyojondev <nabijonovdeveloper@gmail.com>
2024-03-29 10:12:00 +0500

    hello

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./pages/home/Home"
+import Store from "./pages/store/Store"
+import Ipad from "./pages/ipad//Ipad"
+import Iphone from "./pages/iphone/Iphone"
+import Watch from "./pages/watch/Watch"
+import Vision from "./pages/vision/Vision"
+import Mac from "./pages/mac/Mac"
+import {Routes , Route , Link} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* <Link to={"/"}>Home</Link>
+      <Link to={"/store"}>Store</Link>
+      <Link to={"/ipad"}>Ipad</Link>
+      <Link to={"/iphone"}>Iphone</Link>
+      <Link to={"/watch"}>Watch</Link>
+      <Link to={"/vision"}>Vision</Link>
+      <Link to={"/mac"}>Mac</Link> */}
+      <Routes>
+        <Route path='/'element={<Home/>}/>
+        <Route path='/store'element={<Store/>}/>
+        <Route path='/ipad'element={<Ipad/>}/>
+        <Route path='/iphone'element={<Iphone/>}/>
+        <Route path='/watch'element={<Watch/>}/>
+        <Route path='/./vision'element={<Vision/>}/>
+        <Route path='/mac'element={<Mac/>}/>
+      </Routes>
     </div>
   );
 }
