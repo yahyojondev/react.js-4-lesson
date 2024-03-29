4dc4e82cc3448d3fa08724496ecca0f184e3b652
yahyojondev <nabijonovdeveloper@gmail.com>
2024-03-29 11:42:55 +0500

    hello

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import "./Home.css"
 function Home() {
   return (
-    <div>
-        <h2>Home</h2>
+    <div className='home'>
+        <div className="container">
+          <div className="home__wrapper">
+             <h1 className="home__title">
+                MacBook Air
+             </h1>
+             <p className="home__text">Lean.Mean.M3.machine</p>
+             <div className="form__wrapper">
+              <button className="home__btn">Learn more</button>
+              <button className="home__btn">Buy</button>
+             </div>
+          </div>
+        </div>
     </div>
   )
 }
---
+++ src/static/index.js
@@ -0,0 +1,12 @@
+ const home =[
+    
+          {
+      id:2,
+     title:"MacBook Air",
+     text:"Lean.Mean.M3 machine.",
+      link:"Learn more >",
+      link1:"Buy >",
+      bg:"https://www.apple.com/v/home/bl/images/promos/macbook-air-m3/promo_macbook_air_m3__e43jegok3wuq_large.jpg"
+   },
+    
+ ]
